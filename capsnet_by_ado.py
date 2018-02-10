bb0896094f30caf5aaf8350e19ae6448e3d6b31f
AdonisHan <30692767+AdonisHan@users.noreply.github.com>
2018-02-11 06:32:00 +0900

    capsnet in keras

---
+++ capsnet_by_ado.py
@@ -0,0 +1,343 @@
+import numpy as np
+import os
+import pandas as pd
+from keras.preprocessing.image import ImageDataGenerator
+from keras import callbacks
+from keras.utils.vis_utils import plot_model
+import keras.backend as K
+import tensorflow as tf
+from keras import initializers, layers
+from keras import layers, models
+from keras import backend as K
+from keras.utils import to_categorical
+
+
+class Length(layers.Layer):
+    """
+    Compute the length of vectors. This is used to compute a Tensor that has the same shape with y_true in margin_loss
+    inputs: shape=[dim_1, ..., dim_{n-1}, dim_n]
+    output: shape=[dim_1, ..., dim_{n-1}]
+    """
+
+    def call(self, inputs, **kwargs):
+        return K.sqrt(K.sum(K.square(inputs), -1))
+
+    def compute_output_shape(self, input_shape):
+        return input_shape[:-1]
+
+
+class Mask(layers.Layer):
+    """
+    Mask a Tensor with shape=[None, d1, d2] by the max value in axis=1.
+    Output shape: [None, d2]
+    """
+
+    def call(self, inputs, **kwargs):
+        # use true label to select target capsule, shape=[batch_size, num_capsule]
+        if type(inputs) is list:  # true label is provided with shape = [batch_size, n_classes], i.e. one-hot code.
+            assert len(inputs) == 2
+            inputs, mask = inputs
+        else:  # if no true label, mask by the max length of vectors of capsules
+            x = inputs
+            # Enlarge the range of values in x to make max(new_x)=1 and others < 0
+            x = (x - K.max(x, 1, True)) / K.epsilon() + 1
+            mask = K.clip(x, 0, 1)  # the max value in x clipped to 1 and other to 0
+
+        # masked inputs, shape = [batch_size, dim_vector]
+        inputs_masked = K.batch_dot(inputs, mask, [1, 1])
+        return inputs_masked
+
+    def compute_output_shape(self, input_shape):
+        if type(input_shape[0]) is tuple:  # true label provided
+            return tuple([None, input_shape[0][-1]])
+        else:
+            return tuple([None, input_shape[-1]])
+
+
+def squash(vectors, axis=-1):
+    """
+    The non-linear activation used in Capsule. It drives the length of a large vector to near 1 and small vector to 0
+    :param vectors: some vectors to be squashed, N-dim tensor
+    :param axis: the axis to squash
+    :return: a Tensor with same shape as input vectors
+    """
+    s_squared_norm = K.sum(K.square(vectors), axis, keepdims=True)
+    scale = s_squared_norm / (1 + s_squared_norm) / K.sqrt(s_squared_norm)
+    return scale * vectors
+
+
+class CapsuleLayer(layers.Layer):
+    """
+    The capsule layer. It is similar to Dense layer. Dense layer has `in_num` inputs, each is a scalar, the output of the
+    neuron from the former layer, and it has `out_num` output neurons. CapsuleLayer just expand the output of the neuron
+    from scalar to vector. So its input shape = [None, input_num_capsule, input_dim_capsule] and output shape = \
+    [None, num_capsule, dim_capsule]. For Dense Layer, input_dim_capsule = dim_capsule = 1.
+
+    :param num_capsule: number of capsules in this layer
+    :param dim_capsule: dimension of the output vectors of the capsules in this layer
+    :param routings: number of iterations for the routing algorithm
+    """
+
+    def __init__(self, num_capsule, dim_capsule, routings=3,
+                 kernel_initializer='glorot_uniform',
+                 **kwargs):
+        super(CapsuleLayer, self).__init__(**kwargs)
+        self.num_capsule = num_capsule
+        self.dim_capsule = dim_capsule
+        self.routings = routings
+        self.kernel_initializer = initializers.get(kernel_initializer)
+
+    def build(self, input_shape):
+        assert len(input_shape) >= 3, "The input Tensor should have shape=[None, input_num_capsule, input_dim_capsule]"
+        self.input_num_capsule = input_shape[1]
+        self.input_dim_capsule = input_shape[2]
+
+        # Transform matrix
+        self.W = self.add_weight(shape=[self.num_capsule, self.input_num_capsule,
+                                        self.dim_capsule, self.input_dim_capsule],
+                                 initializer=self.kernel_initializer,
+                                 name='W')
+
+        self.built = True
+
+    def call(self, inputs, training=None):
+        # inputs.shape=[None, input_num_capsule, input_dim_capsule]
+        # inputs_expand.shape=[None, 1, input_num_capsule, input_dim_capsule]
+        inputs_expand = K.expand_dims(inputs, 1)
+
+        # Replicate num_capsule dimension to prepare being multiplied by W
+        # inputs_tiled.shape=[None, num_capsule, input_num_capsule, input_dim_capsule]
+        inputs_tiled = K.tile(inputs_expand, [1, self.num_capsule, 1, 1])
+
+        # Compute `inputs * W` by scanning inputs_tiled on dimension 0.
+        # x.shape=[num_capsule, input_num_capsule, input_dim_capsule]
+        # W.shape=[num_capsule, input_num_capsule, dim_capsule, input_dim_capsule]
+        # Regard the first two dimensions as `batch` dimension,
+        # then matmul: [input_dim_capsule] x [dim_capsule, input_dim_capsule]^T -> [dim_capsule].
+        # inputs_hat.shape = [None, num_capsule, input_num_capsule, dim_capsule]
+        inputs_hat = K.map_fn(lambda x: K.batch_dot(x, self.W, [2, 3]), elems=inputs_tiled)
+
+        # Begin: Routing algorithm ---------------------------------------------------------------------#
+        # The prior for coupling coefficient, initialized as zeros.
+        # b.shape = [None, self.num_capsule, self.input_num_capsule].
+        b = tf.zeros(shape=[K.shape(inputs_hat)[0], self.num_capsule, self.input_num_capsule])
+
+        assert self.routings > 0, 'The routings should be > 0.'
+        for i in range(self.routings):
+            # c.shape=[batch_size, num_capsule, input_num_capsule]
+            c = tf.nn.softmax(b, dim=1)
+
+            # c.shape =  [batch_size, num_capsule, input_num_capsule]
+            # inputs_hat.shape=[None, num_capsule, input_num_capsule, dim_capsule]
+            # The first two dimensions as `batch` dimension,
+            # then matmal: [input_num_capsule] x [input_num_capsule, dim_capsule] -> [dim_capsule].
+            # outputs.shape=[None, num_capsule, dim_capsule]
+            outputs = squash(K.batch_dot(c, inputs_hat, [2, 2]))  # [None, 10, 16]
+
+            if i < self.routings - 1:
+                # outputs.shape =  [None, num_capsule, dim_capsule]
+                # inputs_hat.shape=[None, num_capsule, input_num_capsule, dim_capsule]
+                # The first two dimensions as `batch` dimension,
+                # then matmal: [dim_capsule] x [input_num_capsule, dim_capsule]^T -> [input_num_capsule].
+                # b.shape=[batch_size, num_capsule, input_num_capsule]
+                b += K.batch_dot(outputs, inputs_hat, [2, 3])
+        # End: Routing algorithm -----------------------------------------------------------------------#
+
+        return outputs
+
+    def compute_output_shape(self, input_shape):
+        return tuple([None, self.num_capsule, self.dim_capsule])
+
+
+def PrimaryCap(inputs, dim_vector, n_channels, kernel_size, strides, padding):
+    """
+    Apply Conv2D `n_channels` times and concatenate all capsules
+    :param inputs: 4D tensor, shape=[None, width, height, channels]
+    :param dim_vector: the dim of the output vector of capsule
+    :param n_channels: the number of types of capsules
+    :return: output tensor, shape=[None, num_capsule, dim_vector]
+    """
+    output = layers.Conv2D(filters=dim_vector * n_channels, kernel_size=kernel_size, strides=strides, padding=padding)(
+        inputs)
+    outputs = layers.Reshape(target_shape=[-1, dim_vector])(output)
+    return layers.Lambda(squash)(outputs)
+
+###########Build Model
+
+def CapsNet(input_shape, n_class, num_routing):
+    """
+    A Capsule Network on MNIST.
+    :param input_shape: data shape, 4d, [None, width, height, channels]
+    :param n_class: number of classes
+    :param num_routing: number of routing iterations
+    :return: A Keras Model with 2 inputs and 2 outputs
+    """
+    x = layers.Input(shape=input_shape)
+
+    # Layer 1: Just a conventional Conv2D layer
+    conv1 = layers.Conv2D(filters=256, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv1')(x)
+
+    # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_vector]
+    primarycaps = PrimaryCap(conv1, dim_vector=8, n_channels=32, kernel_size=9, strides=2, padding='valid')
+
+    # Layer 3: Capsule layer. Routing algorithm works here.
+    digitcaps = CapsuleLayer(num_capsule=n_class, dim_vector=16, num_routing=num_routing, name='digitcaps')(primarycaps)
+
+    # Layer 4: This is an auxiliary layer to replace each capsule with its length. Just to match the true label's shape.
+    # If using tensorflow, this will not be necessary. :)
+    out_caps = Length(name='out_caps')(digitcaps)
+
+    # Decoder network.
+    y = layers.Input(shape=(n_class,))
+    masked = Mask()([digitcaps, y])  # The true label is used to mask the output of capsule layer.
+    x_recon = layers.Dense(512, activation='relu')(masked)
+    x_recon = layers.Dense(1024, activation='relu')(x_recon)
+    x_recon = layers.Dense(784, activation='sigmoid')(x_recon)
+    x_recon = layers.Reshape(target_shape=[28, 28, 1], name='out_recon')(x_recon)
+
+    # two-input-two-output keras Model
+    return models.Model([x, y], [out_caps, x_recon])
+
+# Margin loss
+def margin_loss(y_true, y_pred):
+    """
+    Margin loss for Eq.(4). When y_true[i, :] contains not just one `1`, this loss should work too. Not test it.
+    :param y_true: [None, n_classes]
+    :param y_pred: [None, num_capsule]
+    :return: a scalar loss value.
+    """
+    L = y_true * K.square(K.maximum(0., 0.9 - y_pred)) + \
+        0.5 * (1 - y_true) * K.square(K.maximum(0., y_pred - 0.1))
+
+    return K.mean(K.sum(L, 1))
+
+
+# define model
+model = CapsNet(input_shape=[28, 28, 1],
+                n_class=10,
+                num_routing=3)
+model.summary()
+try:
+    plot_model(model, to_file='model.png', show_shapes=True)
+except Exception as e:
+    print('No fancy plot {}'.format(e))
+
+########### Load MNIST DATA
+from sklearn.model_selection import train_test_split
+data_train = pd.read_csv('./train.csv')
+X_full = data_train.iloc[:,1:]
+y_full = data_train.iloc[:,:1]
+x_train, x_test, y_train, y_test = train_test_split(X_full, y_full, test_size = 0.3)
+
+x_train = x_train.values.reshape(-1, 28, 28, 1).astype('float32') / 255.
+x_test = x_test.values.reshape(-1, 28, 28, 1).astype('float32') / 255.
+y_train = to_categorical(y_train.astype('float32'))
+y_test = to_categorical(y_test.astype('float32'))
+
+def train(model, data, epoch_size_frac=1.0):
+    """
+    Training a CapsuleNet
+    :param model: the CapsuleNet model
+    :param data: a tuple containing training and testing data, like `((x_train, y_train), (x_test, y_test))`
+    :param args: arguments
+    :return: The trained model
+    """
+    # unpacking the data
+    (x_train, y_train), (x_test, y_test) = data
+
+    # callbacks
+    log = callbacks.CSVLogger('log.csv')
+    checkpoint = callbacks.ModelCheckpoint('weights-{epoch:02d}.h5',
+                                           save_best_only=True, save_weights_only=True, verbose=1)
+    lr_decay = callbacks.LearningRateScheduler(schedule=lambda epoch: 0.001 * np.exp(-epoch / 10.))
+
+    # compile the model
+    model.compile(optimizer='adam',
+                  loss=[margin_loss, 'mse'],
+                  loss_weights=[1., 0.0005],
+                  metrics={'out_caps': 'accuracy'})
+
+    """
+    # Training without data augmentation:
+    model.fit([x_train, y_train], [y_train, x_train], batch_size=args.batch_size, epochs=args.epochs,
+              validation_data=[[x_test, y_test], [y_test, x_test]], callbacks=[log, tb, checkpoint])
+    """
+
+    # -----------------------------------Begin: Training with data augmentation -----------------------------------#
+    def train_generator(x, y, batch_size, shift_fraction=0.):
+        train_datagen = ImageDataGenerator(width_shift_range=shift_fraction,
+                                           height_shift_range=shift_fraction)  # shift up to 2 pixel for MNIST
+        generator = train_datagen.flow(x, y, batch_size=batch_size)
+        while 1:
+            x_batch, y_batch = generator.next()
+            yield ([x_batch, y_batch], [y_batch, x_batch])
+
+    # Training with data augmentation. If shift_fraction=0., also no augmentation.
+    model.fit_generator(generator=train_generator(x_train, y_train, 64, 0.1),
+                        steps_per_epoch=int(epoch_size_frac*y_train.shape[0] / 64),
+                        epochs=1,
+                        validation_data=[[x_test, y_test], [y_test, x_test]],
+                        callbacks=[log, checkpoint, lr_decay])
+    # -----------------------------------End: Training with data augmentation -----------------------------------#
+
+    model.save_weights('trained_model.h5')
+    print('Trained model saved to \'trained_model.h5\'')
+
+    return model
+
+
+######### Combine image & Test #######
+def combine_images(generated_images):
+    num = generated_images.shape[0]
+    width = int(np.sqrt(num))
+    height = int(np.ceil(float(num)/width))
+    shape = generated_images.shape[1:3]
+    image = np.zeros((height*shape[0], width*shape[1]),
+                     dtype=generated_images.dtype)
+    for index, img in enumerate(generated_images):
+        i = int(index/width)
+        j = index % width
+        image[i*shape[0]:(i+1)*shape[0], j*shape[1]:(j+1)*shape[1]] = \
+            img[:, :, 0]
+    return image
+
+def test(model, data):
+    x_test, y_test = data
+    y_pred, x_recon = model.predict([x_test, y_test], batch_size=100)
+    print('-'*50)
+    print('Test acc:', np.sum(np.argmax(y_pred, 1) == np.argmax(y_test, 1))/y_test.shape[0])
+
+    import matplotlib.pyplot as plt
+    from PIL import Image
+
+    img = combine_images(np.concatenate([x_test[:50],x_recon[:50]]))
+    image = img * 255
+    Image.fromarray(image.astype(np.uint8)).save("real_and_recon.png")
+    print()
+    print('Reconstructed images are saved to ./real_and_recon.png')
+    print('-'*50)
+    plt.imshow(plt.imread("real_and_recon.png", ))
+    plt.show()
+
+
+########### Train Capsnet############
+train(model=model, data=((x_train, y_train), (x_test[:60], y_test[:60])),
+      epoch_size_frac = 0.5) # do 10% of an epoch (takes too long)
+
+########### Show results on the hold-out (Test) ########
+# test(model=model, data=(x_test[:100], y_test[:100]))
+
+########### Apply Model to the Competition Data ##########
+'''
+data_test = pd.read_csv('./test.csv')
+data_test = data_test.values.reshape(-1, 28, 28, 1).astype('float32') / 255.
+
+y_pred, _ = model.predict([data_test,
+                           np.zeros((data_test.shape[0],10))], # empty values for the second vector
+                           batch_size = 32, verbose = True)
+                           
+with open('submission.csv', 'w') as out_file:
+    out_file.write('ImageId,Label\n')
+    for img_id, guess_label in enumerate(np.argmax(y_pred,1),1):
+        out_file.write('%d,%d\n' % (img_id, guess_label))
+'''
